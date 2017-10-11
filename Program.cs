7b6c2f3914bcde1fb1287f543a713c2e0cef1996
FIC <alandermm@hotmail.com>
2017-10-11 20:31:12 -0300

    Projeto cadastro cliente

--- Program.cs
+++ Program.cs
@@ -3,17 +3,15 @@ using System.IO;
 
 namespace codexpaula6
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    class Program {
+        static void Main(string[] args) {
             string[] perguntas = File.ReadAllLines(@"C:\Users\FIC\Desktop\codexpaula6\perguntas.txt");
             string[] respostas = new string[perguntas.Length];
             StreamWriter arquivo_respostas = new StreamWriter(@"C:\Users\FIC\Desktop\codexpaula6\respostas.csv", true);
             fazerPerguntas(perguntas, respostas, arquivo_respostas);
             arquivo_respostas.Close();
         }
-        static void fazerPerguntas(string[] perguntas, string[] respostas, StreamWriter arquivo_respostas){
+        static void fazerPerguntas(string[] perguntas, string[] respostas, StreamWriter arquivo_respostas) {
             for(int i = 0; i < perguntas.Length; i++){
                 Console.Write("Qual " + perguntas[i] + ": ");
                 respostas[i] = Console.ReadLine();
@@ -24,14 +22,13 @@ namespace codexpaula6
             if (novo == "sim" || novo == "Sim" || novo == "s" || novo == "S" )
                 fazerPerguntas(perguntas, respostas, arquivo_respostas);
         }
-        static void escreverRespostas(string[] respostas, StreamWriter arquivo_respostas){
+        static void escreverRespostas(string[] respostas, StreamWriter arquivo_respostas) {
             for(int i = 0; i < respostas.Length; i++){
                 if (i == (respostas.Length - 1))
                     arquivo_respostas.Write(respostas[i] + "\n");
                 else
                     arquivo_respostas.Write(respostas[i] + ";");
             }
-            
         }
     }
 }
